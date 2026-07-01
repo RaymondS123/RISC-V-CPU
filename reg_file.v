d7fc9ed739e1da892d912aef03e8d0f9c6e28662
Raymond <Raymondshan2006@gmail.com>
2026-06-30 23:50:34 -0400

    reg_file

--- reg_file.v
+++ reg_file.v
@@ -11,7 +11,7 @@ module reg_file(
 
     //write port
     input wire [4:0] rd,
-    output wire [31:0] rd,
+    output wire [31:0] wd,
     input wire we //write enable
 );
 
@@ -19,8 +19,22 @@ module reg_file(
 reg [31:0] regs [31:0];
 
 integer i;
-always @(*) begin
+
+initial begin
     for(i = 0; i < 32; i = i + 1) begin
         regs[i] = 32'b0;
     end
 end
+
+//synchronous write
+always @(posedge clk) begin
+    if(we && rd != 5'b0) begin
+        regs[rd] <= wd;
+    end
+end
+
+//asynchronous read
+assign rd1 = (rs1 == 5'b0) ? 32'b0 : regs[rs1];
+assign rd2 = (rs2 == 5'b0) ? 32'b0 : regs[rs2];
+
+endmodule
